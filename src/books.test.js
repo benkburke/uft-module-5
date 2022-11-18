29969fd58d45e54ac5920c1f35e6e0bbb8c1c037
Ben Burke <benkburke@gmail.com>
2022-11-18 00:13:28 +0000

    Update 4 files

--- src/books.test.js
+++ src/books.test.js
@@ -20,21 +20,21 @@ beforeEach(async () => {
         bookId: 21601,
         name: "Wind in the willows",
         ownerId: "bburke@greencheckverified.com",
-        author: "Kenneth Graeme"
+        author: "Kenneth Graeme",
       },
       {
         bookId: 21611,
         name: "I, Robot",
         ownerId: "bburke@greencheckverified.com",
-        author: "Isaac Asimov"
+        author: "Isaac Asimov",
       },
       {
         bookId: 21621,
         name: "The Hobbit",
         ownerId: "bburke@greencheckverified.com",
-        author: "Jrr Tolkein"
-      }
-    ]
+        author: "Jrr Tolkein",
+      },
+    ],
   };
 
   allBooksStub = {
@@ -44,33 +44,33 @@ beforeEach(async () => {
         bookId: 31,
         name: "Moby Dick",
         ownerId: null,
-        author: "Herman Melville"
+        author: "Herman Melville",
       },
       {
         bookId: 41,
         name: "The Art of War",
         ownerId: null,
-        author: "Sun Tzu"
+        author: "Sun Tzu",
       },
       {
         bookId: 21601,
         name: "Wind in the willows",
         ownerId: "bburke@greencheckverified.com",
-        author: "Kenneth Graeme"
+        author: "Kenneth Graeme",
       },
       {
         bookId: 21611,
         name: "I, Robot",
         ownerId: "bburke@greencheckverified.com",
-        author: "Isaac Asimov"
+        author: "Isaac Asimov",
       },
       {
         bookId: 21621,
         name: "The Hobbit",
         ownerId: "bburke@greencheckverified.com",
-        author: "Jrr Tolkein"
-      }
-    ]
+        author: "Jrr Tolkein",
+      },
+    ],
   };
 
   httpGateway.get = jest.fn().mockImplementation((path) => {
@@ -95,28 +95,72 @@ let setup = async (userMode) => {
   });
 };
 
-it("should load private books", async () => {
-  await setup("private");
+describe("load books usecase", () => {
+  it("should load private books by default", async () => {
+    await setup("");
 
-  expect(httpGateway.get).toHaveBeenCalledWith(
-    "https://api.logicroom.co/api/bburke@greencheckverified.com/books"
-  );
-  expect(viewModel.length).toBe(3);
-  expect(viewModel[0].name).toBe("Wind in the willows");
-  expect(viewModel[1].name).toBe("I, Robot");
-  expect(viewModel[2].name).toBe("The Hobbit");
+    expect(httpGateway.get).toHaveBeenCalledWith(
+      "https://api.logicroom.co/api/bburke@greencheckverified.com/books"
+    );
+    expect(viewModel.length).toBe(3);
+    expect(viewModel[0].name).toBe("Wind in the willows");
+    expect(viewModel[1].name).toBe("I, Robot");
+    expect(viewModel[2].name).toBe("The Hobbit");
+  });
+
+  it("should load private books", async () => {
+    await setup("private");
+
+    expect(httpGateway.get).toHaveBeenCalledWith(
+      "https://api.logicroom.co/api/bburke@greencheckverified.com/books"
+    );
+    expect(viewModel.length).toBe(3);
+    expect(viewModel[0].name).toBe("Wind in the willows");
+    expect(viewModel[1].name).toBe("I, Robot");
+    expect(viewModel[2].name).toBe("The Hobbit");
+  });
+
+  it("should load public books", async () => {
+    await setup("public");
+
+    expect(httpGateway.get).toHaveBeenCalledWith(
+      "https://api.logicroom.co/api/bburke@greencheckverified.com/allbooks"
+    );
+    expect(viewModel.length).toBe(5);
+    expect(viewModel[0].name).toBe("Moby Dick");
+    expect(viewModel[1].name).toBe("The Art of War");
+    expect(viewModel[2].name).toBe("Wind in the willows");
+    expect(viewModel[3].name).toBe("I, Robot");
+    expect(viewModel[4].name).toBe("The Hobbit");
+  });
 });
 
-it("should load public books", async () => {
-  await setup("public");
+describe("sort books usecase", () => {
+  it("should sort ascending on name when user chooses to", async () => {
+    await setup("private");
 
-  expect(httpGateway.get).toHaveBeenCalledWith(
-    "https://api.logicroom.co/api/bburke@greencheckverified.com/allbooks"
-  );
-  expect(viewModel.length).toBe(5);
-  expect(viewModel[0].name).toBe("Moby Dick");
-  expect(viewModel[1].name).toBe("The Art of War");
-  expect(viewModel[2].name).toBe("Wind in the willows");
-  expect(viewModel[3].name).toBe("I, Robot");
-  expect(viewModel[4].name).toBe("The Hobbit");
+    await booksPresenter.sortBooks("asc");
+
+    expect(httpGateway.get).toHaveBeenCalledWith(
+      "https://api.logicroom.co/api/bburke@greencheckverified.com/books"
+    );
+    expect(viewModel.length).toBe(3);
+    expect(viewModel[0].name).toBe("I, Robot");
+    expect(viewModel[1].name).toBe("The Hobbit");
+    expect(viewModel[2].name).toBe("Wind in the willows");
+  });
+
+  it("should sort descending on name when user chooses to", async () => {
+    await setup("private");
+
+    await booksPresenter.sortBooks("desc");
+
+    expect(httpGateway.get).toHaveBeenCalledWith(
+      "https://api.logicroom.co/api/bburke@greencheckverified.com/books"
+    );
+    expect(viewModel.length).toBe(3);
+    expect(viewModel[0].name).toBe("Wind in the willows");
+    expect(viewModel[1].name).toBe("The Hobbit");
+    expect(viewModel[2].name).toBe("I, Robot");
+  });
 });
